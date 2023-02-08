10e821a373c4e475998cc5fd0ee10ff9c1cd55a5
Wezaino <116382837+Wezaino@users.noreply.github.com>
2023-02-08 17:43:57 +0100

    Add files via upload

---
+++ spr.js
@@ -0,0 +1,15 @@
+const text = document.querySelector('#colorText');
+
+setInterval(function() {
+  const color = getRandomColor();
+  text.style.color = color;
+}, 1000);
+
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
